8853b6de0b5a5a873bc548e41cb62fd185736a8f
salih poozhithara <salihpoozhitharaa@gmail.com>
2024-10-24 00:13:34 +0530

    Finished place order page / delivery info

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -69,6 +69,10 @@ const Cart = () => {
       <div className='flex justify-end my-20'>
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
+
+          <div className='w-full text-end'>
+            <button className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+          </div>
         </div>
       </div>
       
