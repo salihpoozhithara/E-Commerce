3e2186a20a87e5b84b904b2c856dc8456d7a1eed
salih poozhithara <salihpoozhitharaa@gmail.com>
2024-10-31 00:35:09 +0530

    designed login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List';
 import Orders from './Pages/Orders';
+import Login from './components/Login'
 
 const App = () => {
+  // when we are not authenticated then we will displaylogin component
+    const [token,setToken] =useState('')
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
-        <Navbar/>
-        <hr />
-        <div className='flex w-full'>
-          <Sidebar />
-          <div>
-            <Routes>
-              <Route path='/add' element={<Add />} />
-              <Route path='/list' element={<List />} />
-              <Route path='/orders' element={<Orders />} />
-            </Routes>
-          </div>
+      {/* check token not availbla logib component will display*/}
+      { token === "" 
+      ? <Login /> 
+      :<>
+      <Navbar/>
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar />
+        <div>
+          <Routes>
+            <Route path='/add' element={<Add />} />
+            <Route path='/list' element={<List />} />
+            <Route path='/orders' element={<Orders />} />
+          </Routes>
         </div>
+      </div>
+    
+    </> 
+    }
       
-      </>
     </div>
   )
 }
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Add = () => {
   return (
     <div>
-        gvgvgg
+        
     </div>
   )
 }
